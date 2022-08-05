a07ae710479ea5cf4183ec52071d8868c2d83cf5
meetIgor <iigoor.s@gmail.com>
2022-08-05 17:01:27 +0300

    Initial Commit

---
+++ SwiftBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftBook
+//
+//  Created by igor s on 05.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
