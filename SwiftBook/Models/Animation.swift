723d46cb78588cadb027dad1dcb8af9e9a5eea2b
meetIgor <iigoor.s@gmail.com>
2022-08-05 22:01:22 +0300

    random animation app, using Spring Animation

---
+++ SwiftBook/Models/Animation.swift
@@ -0,0 +1,42 @@
+//
+//  Animation.swift
+//  SwiftBook
+//
+//  Created by igor s on 05.08.2022.
+//
+
+import Foundation
+import SpringAnimation
+
+struct Animation {
+    let title: String
+    let force: Double
+    let curve: String
+    let duration: Double
+    let delay: Double
+    
+    static func getDefault() -> Animation {
+        Animation(
+            title: "pop",
+            force: 1,
+            curve: "easeIn",
+            duration: 1.8,
+            delay: 0.4
+        )
+    }
+    
+    static func getRandomAnimation() -> Animation {
+        
+        let title = AnimationPreset.allCases.randomElement()?.rawValue ?? "pop"
+        let curve = AnimationCurve.allCases.randomElement()?.rawValue ?? "easeIn"
+        
+        let animation = Animation(
+            title: title,
+            force: Double.random(in: 1.0...5.0),
+            curve: curve,
+            duration: Double.random(in: 0.5...5.0),
+            delay: Double.random(in: 0.0...5.0)
+        )
+        return animation
+    }
+}
---
+++ SwiftBook/ViewControllers/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  SwiftBook
+//
+//  Created by igor s on 05.08.2022.
+//
+
+import UIKit
+import SpringAnimation
+
+class ViewController: UIViewController {
+    //MARK: - IB Outlets
+    @IBOutlet weak var springView: SpringView!
+    
+    @IBOutlet weak var presetLabel: UILabel!
+    @IBOutlet weak var curveLabel: UILabel!
+    @IBOutlet weak var forceLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var delayLabel: UILabel!
+    
+    @IBOutlet weak var runButton: UIButton!
+    
+    //MARK: - Private Properties
+    private var animation = Animation.getDefault()
+    
+    //MARK: - Life Cycles Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setLabeles(with: animation)
+    }
+    
+    //MARK: - IB Actions
+    @IBAction func runButtonPressed(_ sender: UIButton) {
+        setAnimationValues(for: animation)
+        setLabeles(with: animation)
+        springView.animate()
+        animation = Animation.getRandomAnimation()
+        runButton.setTitle("Run: \(animation.title)", for: .normal)
+    }
+    
+    //MARK: - Private Properties
+    private func setLabeles(with animation: Animation) {
+        presetLabel.text = animation.title
+        curveLabel.text = animation.curve
+        forceLabel.text = String(format: "%.1f", animation.force)
+        durationLabel.text = String(format: "%.1f", animation.duration)
+        delayLabel.text = String(format: "%.1f", animation.delay)
+    }
+    
+    private func setAnimationValues(for animation: Animation) {
+        springView.animation = animation.title
+        springView.curve = animation.curve
+        springView.force = animation.force
+        springView.duration = animation.duration
+        springView.delay = animation.delay
+    }
+}
+
